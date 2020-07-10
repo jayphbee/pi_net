d9876716fec2a7788429edb74846d25f9400a4cd
jiangfb <1209467293@qq.com>
2020-07-10 16:52:17 +0800

    api breaking change

--- tcp/src/util.rs
+++ tcp/src/util.rs
@@ -117,13 +117,13 @@ impl SocketReady {
         if ready.is_readable() && ready.is_writable() {
             self.0.store(0, Ordering::SeqCst);
         } else if ready.is_readable() {
-            self.0.fetch_update(|v| {
+            self.0.fetch_update(Ordering::SeqCst, Ordering::SeqCst, |v| {
                 Some(v & !1)
-            }, Ordering::SeqCst, Ordering::SeqCst);
+            });
         } else if ready.is_writable() {
-            self.0.fetch_update(|v| {
+            self.0.fetch_update(Ordering::SeqCst, Ordering::SeqCst, |v| {
                 Some(v & !2)
-            }, Ordering::SeqCst, Ordering::SeqCst);
+            });
         }
     }
 }
